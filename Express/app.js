be10e64b93cee453499abf00cbe749e84f056faf
Shyamal <sgprajapati24@gmail.com>
2022-03-13 21:27:33 +0530

    Express learnings started

--- Express/app.js
+++ Express/app.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var app = express();
+app.set('view engine', 'ejs');
+
+// const cors = require('cors');
+// app.use(cors());
 
 app.get('/',function(req, res){
-    res.send('You are at the home page');
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/help', function(req, res){
-    res.send('You are at the help page');
+app.get('/grade', function(req, res){
+    res.sendFile(__dirname + '/grade.html');
 });
+app.get('/admin', (req, res) => {
+    res.sendFile(__dirname + '/admin.html');
+  });
+
+app.get('/profile/:name', function(req, res) {
+    // res.send('You are at the profile page ' + req.params.id);
+    res.render('profile', {person: req.params.name});
+});
+
 
 app.listen(4000);
